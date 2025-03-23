104ea10ae6ade9114e8a0fb4f38d9cb0f87ec23d
Srishti Singh <srish17816@gmail.com>
2025-03-23 13:59:27 +0000

    images folder created

---
+++ DOM/1.js
@@ -0,0 +1,50 @@
+// DOM => Document Object Model
+
+// HTML me aisa koi feature hi nahi hai jis se tum button pe click kar ke div gayab nahi kar sakte
+
+// JS ka aisa koi bhi code jo webpage me koi bhi change karega wo js ka DOM ka code kehlaega.
+
+// DOM Manipulation => HTML ki body me kuch bhi change karna with DOM 
+
+// uss-se      ye kar do   ye hone par
+// selection   change      event
+
+
+
+// Selection :- can be done with following ways:-
+
+// document.getElementById .... ye sirf ID ko select karta hai
+// document.getElementsByClassName .... ye sirf ClassName ko select karta hai
+// document.getElementsByTagName .... ye sirf TagName ko select karta hai
+
+// pr ek aisa hai jo sab select kar sakta hai(ye teeno select kar sakta hai):-
+
+// document.querySelector 
+
+
+
+// Selections of :-
+
+// 1. ID (id name : myid):- 
+
+// var abc = document.getElementById(`#myid`)
+
+
+// 2. Class (class : myclass):- 
+
+// var abc = document.getElementById(`.myclass`)
+
+
+// 3. Tag (tag name : mytag):- 
+
+// var abc = document.getElementById(`mytag`)
+
+
+
+
+
+
+
+
+
+
